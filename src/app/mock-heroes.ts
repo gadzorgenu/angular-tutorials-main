0ff8bf39fa3cf57beaf7bf84c11065e332cf7f97
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-12-12 23:36:33 +0000

    Added editor and display feature

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import { Hero} from './Hero'
+
+export const HEROES: Hero[] = [
+    {id:1, name:'Gina'},
+    {id:2, name:'Georgina'},
+    {id:3, name:'Adz'},
+    {id:4, name:'Ga'},
+    {id:5, name:'Giiinaa'},
+    {id:6, name:'Narco'},
+    {id:7, name:'Magneta'},
+    {id:8, name:'Magma'},
+    {id:9, name:'Lava'},
+    {id:10, name:'Tornado'},
+    {id:11, name:'Nice'},
+    {id:12, name:'Noma'},
+    {id:13, name:'Meme'},
+    {id:14, name:'Nene'},
+    {id:15, name:'Nana'},
+]
